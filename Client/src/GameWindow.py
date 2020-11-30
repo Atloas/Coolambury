9a716fdc687ab3368144dd3419e57e8433d4016a
Atloas <miter010@gmail.com>
2020-11-30 18:03:24 +0100

    Window transition

--- Client/src/GameWindow.py
+++ Client/src/GameWindow.py
@@ -4,13 +4,15 @@ from PyQt5 import QtCore, QtWidgets, QtGui
 class GameWindow(QtWidgets.QWidget):
     switch_window = QtCore.pyqtSignal()
 
-    def __init__(self, roomNumber):
+    def __init__(self, roomCode):
         QtWidgets.QWidget.__init__(self)
-        self.roomNumber = roomNumber
-        self.setWindowTitle("Coolambury: {}".format(self.roomNumber))
+        self.roomCode = roomCode
+        self.setWindowTitle("Coolambury: {}".format(self.roomCode))
 
         # TODO: Drawing
+        self.vBox = QtWidgets.QVBoxLayout()
         self.label = QtWidgets.QLabel()
         canvas = QtGui.QPixmap(400, 300)
         self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
+
+        self.vBox.addWidget(self.label)
--- Client/src/WindowController.py
+++ Client/src/WindowController.py
@@ -5,17 +5,18 @@ from StartWindow import StartWindow
 class WindowController:
 
     def __init__(self):
-        self.window = None
+        self.startWindow = None
+        self.gameWindow = None
 
     def show_start(self):
-        if self.window is not None:
-            self.window.close()
-        self.window = StartWindow()
-        self.window.switch_window.connect(self.show_start)  # Should be show_game, this is for debugging
-        self.window.show()
+        self.startWindow = StartWindow()
+        self.startWindow.switch_window.connect(self.show_game)  # TODO: Should be show_game, this is for debugging
+        if self.gameWindow is not None:
+            self.gameWindow.close()
+        self.startWindow.show()
 
     def show_game(self, roomNumber):
-        self.window.close()
-        self.window = GameWindow(roomNumber)
-        self.window.switch_window.connect(self.show_start)
-        self.window.show()
+        self.gameWindow = GameWindow(roomNumber)
+        self.gameWindow.switch_window.connect(self.show_start)
+        self.startWindow.close()
+        self.gameWindow.show()
