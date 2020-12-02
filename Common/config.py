0d06eb015deb1d21593284ad48d46dde18966a8f
awysocki <adrian.wyso.97@gmail.com>
2020-12-02 18:12:51 +0100

    add windows common path adding

--- Common/config.py
+++ Common/config.py
@@ -1,3 +1,4 @@
+
 class Config:
     def __init__(self):
         self.PORT = 5050
--- Server/src/common.py
+++ Server/src/common.py
@@ -3,5 +3,12 @@ import sys
 import pathlib
 
 app_path = pathlib.Path().absolute()
-common_path = str(app_path) + '/Common/'
+print(sys.platform)
+
+if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
+    common_path = str(app_path) + '/Common/'
+elif sys.platform == "win32" or sys.platform == "win64":
+    common_path = str(app_path) + '\\Common\\'
+
+
 sys.path.append(common_path)
