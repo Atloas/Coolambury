3a8ad9decf3bac32d85c20c81f15c196d8384b06
awysocki <adrian.wyso.97@gmail.com>
2020-12-17 19:51:31 +0100

    removed common library and made some refactoring

--- Server/messagehandlers/__init__.py
+++ Server/messagehandlers/__init__.py
@@ -1,6 +1,6 @@
 from .createroomreqhandler import CreateRoomReqHandler
 from .joinroomreqhandler import JoinRoomReqHandler
-from .writechatreqhandler import WriteChatReqHandler
+from .chatmessagereqhandler import ChatMessageReqHandler
 
 if __name__ == '__main__':
     pass
