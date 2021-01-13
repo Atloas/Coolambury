633b0d8222d316dc3ad49901bd3f0ceddb17fc5a
Atloas <miter010@gmail.com>
2021-01-13 17:26:34 +0100

    handleOwnerChangedSignal, removed some TODOs

--- Client/Application/AppResourceManager.py
+++ Client/Application/AppResourceManager.py
@@ -28,7 +28,6 @@ class AppResourceManager:
         if roomCode != 'Joining':
             self.clientContext['roomCode'] = roomCode
         self.gameWindow = GameWindow(self.clientContext, self.connHandler)
-        # TODO: Move to GameWindow.__init__()?
         self.gameWindow.switch_window.connect(self.show_start)
         self.startWindow.hide()
         self.gameWindow.show()
